7377da049b10fd4ac879ed53625f24b4111f9507
rifat17 <a.hasib.rifat@gmail.com>
2019-10-10 16:03:01 +0600

    menuV02 going to need routineScrapper.py

--- routineBaiust/routineScrapper.py
+++ routineBaiust/routineScrapper.py
@@ -3,57 +3,61 @@ from bs4 import BeautifulSoup as Soup
 import re
 import datetime
 
-my_url = "http://localhost:8080/routine_21-09-2019_1-03-33.html"
+# my_url = "http://localhost:8080/routine_21-09-2019_1-03-33.html"
 
-uClient = urlopen(my_url)
-page_html = uClient.read()
-uClient.close()
+my_url = 'http://routine.baiust.edu.bd/?department_id=1&semester=12'
 
-page_soup = Soup(page_html,'html.parser')
+def Scraper(my_url):
+    uClient = urlopen(my_url)
+    page_html = uClient.read()
+    uClient.close()
 
-university = page_soup.h2.text
-# print(university)
-dept = page_soup.h4.text
-print(dept)
+    page_soup = Soup(page_html,'html.parser')
 
-
-
-table = page_soup.table
-
-tbody = table.tbody
-
-trClass = {'Sunday' : 'bg-danger text-light',
-         'Monday' : 'bg-info text-light', 
-         'Thuesday' : 'bg-success text-light', 
-         'Wednesday' : 'bg-dark text-light', 
-         'Thursday': 'bg-primary text-light'}
+    university = page_soup.h2.text
+    # print(university)
+    dept = page_soup.h4.text
+    print(dept)
 
 
-# print(datetime.date.strftime(datetime.datetime.today(),'%A'))
-Today = datetime.date.strftime(datetime.datetime.today(),'%A')
 
-routine = page_soup.findAll("tr",{"class" : trClass[Today]})
-# print(routine)
+    table = page_soup.table
 
-for sub in routine:
-    Day = sub.td.b.text
-    subCon = sub.findAll("div",{"class":""})
+    tbody = table.tbody
 
-    # #print(subCon)
-    print(Day)
-    for x in subCon:
-        # print(type(x))
-        # print(len(x))
-        # print(x)
-        course  = x.p.text.strip()
-        teacher = x.span.text
-        room    = x.span.next_sibling.next_sibling.text
-        # section = x.span.next_sibling.next_sibling.find_next_siblings()
-        print(course,teacher,room)
+    trClass = {
+        'Sunday'    : 'bg-danger text-light',
+        'Monday'    : 'bg-info text-light', 
+        'Thuesday'  : 'bg-success text-light', 
+        'Wednesday' : 'bg-dark text-light', 
+        'Thursday'  : 'bg-primary text-light'}
 
 
-    # courseID = subCon[0].text.strip()
-    # teacher,room = subCon[1].text.split(',')
-    # print(Day,'\n',courseID,'\n',teacher,room)
+    # print(datetime.date.strftime(datetime.datetime.today(),'%A'))
+    Today = datetime.date.strftime(datetime.datetime.today(),'%A')
+
+    routine = page_soup.findAll("tr",{"class" : trClass[Today]})
+    # print(routine)
+
+    for sub in routine:
+        Day = sub.td.b.text
+        subCon = sub.findAll("div",{"class":""})
+
+        # #print(subCon)
+        print(Day)
+        for x in subCon:
+            # print(type(x))
+            # print(len(x))
+            # print(x)
+            course  = x.p.text.strip()
+            teacher = x.span.text
+            room    = x.span.next_sibling.next_sibling.text
+            # section = x.span.next_sibling.next_sibling.find_next_siblings()
+            print(course,teacher,room)
+
+
+        # courseID = subCon[0].text.strip()
+        # teacher,room = subCon[1].text.split(',')
+        # print(Day,'\n',courseID,'\n',teacher,room)
 
 
